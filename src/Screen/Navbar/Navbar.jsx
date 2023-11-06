7c88da8d50a938b5123379d72f2ff229a4d639a9
rahulcylsys841412 <140055218+rahulcylsys841412@users.noreply.github.com>
2023-11-06 14:56:12 +0530

    ristaa

---
+++ src/Screen/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+    <div className="container">
+        <div className="row">
+                <div className="col-md-4">Rissta</div>
+                <div className="col-md-2">Home</div>
+                <div className="col-md-2">About</div>
+                <div className="col-md-4">Contact</div>
+        </div>
+    </div>
+    
+    </>
+  )
+}
+
+export default Navbar
