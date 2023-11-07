4c2b1ef6d04fe18b6b51d9554045504f8cd23f39
rahulcylsys841412 <118243747+Rahulkumarsingh1996@users.noreply.github.com>
2023-11-07 20:03:42 +0530

    Rahul singh
    menu nav is apporx done to add some new icon

--- src/Screen/Navbar/Navbar.jsx
+++ src/Screen/Navbar/Navbar.jsx
@@ -1,16 +1,32 @@
 import React from "react";
 import "./Navbar.css";
+
 const Navbar = () => {
   return (
     <>
       <div className="container navbar">
-        <div className="row flex-column g-4 mt-2 menu-box">
+        <div className="row flex-column g-4 mt-1 menu-box">
           <div className="col-md-2 menu-item">Clikr</div>
-          <div className="col-md-2 menu-item">Home</div>
-          <div className="col-md-2 menu-item">Search</div>
-          <div className="col-md-2 menu-item">Reel</div>
-          <div className="col-md-2 menu-item">Message</div>
-          <div className="col-md-1 menu-item">Profile</div>
+          <div className="col-md-2 menu-item m-0 p-0 d-flex align-items-center gap-2">
+            {" "}
+            <i class="fa-solid fa-house"></i>Home
+          </div>
+          <div className="col-md-2 menu-item m-0 p-0 d-flex align-items-center gap-2">
+            {" "}
+            <i class="fa-solid fa-magnifying-glass"></i>Search
+          </div>
+          <div className="col-md-2 menu-item m-0 p-0 d-flex align-items-center gap-2">
+            {" "}
+            <i class="fa-solid fa-circle-play"></i>Reel
+          </div>
+          <div className="col-md-2 menu-item m-0 p-0 d-flex align-items-center gap-2">
+            {" "}
+            <i class="fa-solid fa-comment"></i>Message
+          </div>
+          <div className="col-md-2 menu-item m-0 p-0 d-flex align-items-center gap-2">
+            {" "}
+            <i class="fa-solid fa-user"></i>Profile
+          </div>
         </div>
       </div>
     </>
